44ad7c659ad4a1428612e83b0355a485e59aab68
darkruden <bezerra35eduardo@gmail.com>
2025-11-21 23:07:08 -0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './components/ui/ui.css'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import './styles/global.css'   /* <--- NOSSO ESTILO GLOBAL */
+import './components/ui/ui.css' /* <--- NOSSO UI KIT */
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
